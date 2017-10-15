c29cedcac1d6285dc34e71e3b5ccf8feed125df0
therealplato <therealplato@gmail.com>
2017-10-15 21:27:02 +1300

    meh

---
+++ example/tcg/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	// start rulescape server
+	// nearby players request the rulescape
+	// rulescape explains the protocol for trade requests
+	// endpoint, transport protocol, encoding, data structure
+
+	// start trade server
+	// make have/want list to authorized identities
+	// receive trade requests from nearby players
+	// automatically reject requests from identity Anonymous
+	// persist a record of the trade request (if rulescape permits logging)
+	// notify me that a trade offer is available
+	// I accept or reject
+}
